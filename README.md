cfd8551d9208d63cc21af6e339e09a95fdabd261
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-03-01 16:53:33 -0700

    Initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# SlotMachine App
+Seventh app from the SwiftUI online course
+
+## Main Learnings
+
+- ...
---
+++ SlotMachine/Extensions/Color+Custom.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Custom.swift
+//  SlotMachine
+//
+//  Created by Víctor Manuel Puga Ruiz on 01/03/21.
+//
+
+import SwiftUI
+
+extension Color {
+  static var colorPink: Color { Color("ColorPink") }
+  
+  static var colorPurlple: Color { Color("ColorPurple") }
+  
+  static var colorYellow: Color { Color("ColorYellow") }
+  
+  static var colorTransparentBlack: Color { Color("ColorTransparentBlack") }
+}

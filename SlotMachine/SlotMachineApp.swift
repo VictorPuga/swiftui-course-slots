5f02fb86b5ba63df194a553fb51c1a66c381fccb
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-03-01 16:30:52 -0700

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Víctor Manuel Puga Ruiz on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ad77697200454d74650787df0af7c3e9d00c4510
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-03-21 15:47:39 -0700

    Info view and main game logic

--- SlotMachine/Views/ContentView.swift
+++ SlotMachine/Views/ContentView.swift
@@ -9,8 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
   // MARK: - Properties
+  let symbols: [String] = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
+  
+  @State private var showingInfoView: Bool = false
+  
+  @State private var reels: [Int] = [0, 1, 2]
+  
+  @State private var highscore: Int = 0
+  
+  @State private var coins: Int = 100
+  
+  @State private var betAmount: Int = 10
+  
+  @State private var showingModal: Bool = false
+  
+  var isActiveBet10: Bool { betAmount == 10 }
+  
+  var isActiveBet20: Bool { betAmount == 20 }
   
   
+  // MARK: - Functions
+  
+  func spinReels() {
+    reels = reels.map { _ in Int.random(in: 0..<symbols.count) }
+  }
+  
+  func checkWinning() {
+    let first = reels[0]
+    if reels.allSatisfy({ $0 == first }) {
+      playerWins()
+      
+      if coins > highscore {
+        newHighscore()
+      }
+    } else {
+      playerLoses()
+    }
+  }
+  
+  func playerWins() {
+    coins += betAmount * 10
+  }
+  
+  func newHighscore() {
+    highscore = coins
+  }
+  
+  func playerLoses() {
+    coins -= betAmount
+  }
+  
+  func activateBet(_ amount: Int) {
+    betAmount = amount
+  }
+  
+  // func activateBet10() {
+  //   betAmount = 10
+  // }
+  
+  func isGameOver() {
+    if coins <= 0 {
+      showingModal = true
+    }
+  }
   
   // MARK: - Body
   var body: some View {
@@ -36,7 +97,7 @@ struct ContentView: View {
             Text("Your\nCoins".uppercased())
               .scoreLabelStyle()
               .multilineTextAlignment(.trailing)
-            Text("100")
+            Text("\(coins)")
               .scoreNumberStyle()
               .modifier(ScoreNumberModifier())
           }
@@ -45,7 +106,7 @@ struct ContentView: View {
           Spacer()
           
           HStack {
-            Text("200")
+            Text("\(highscore)")
               .scoreNumberStyle()
               .modifier(ScoreNumberModifier())
             Text("High\nScore".uppercased())
@@ -59,7 +120,7 @@ struct ContentView: View {
           // MARK: Reel #1
           ZStack {
             ReelView()
-            Image("gfx-bell")
+            Image(symbols[reels[0]])
               .resizable()
               .modifier(ImageModifier())
           }
@@ -67,7 +128,7 @@ struct ContentView: View {
             // MARK: Reel #2
             ZStack {
               ReelView()
-              Image("gfx-seven")
+              Image(symbols[reels[1]])
                 .resizable()
                 .modifier(ImageModifier())
             }
@@ -76,7 +137,7 @@ struct ContentView: View {
             // MARK: Reel #3
             ZStack {
               ReelView()
-              Image("gfx-cherry")
+              Image(symbols[reels[2]])
                 .resizable()
                 .modifier(ImageModifier())
             }
@@ -85,7 +146,11 @@ struct ContentView: View {
           .frame(maxWidth: 500)
           
           // MARK: Spin Button
-          Button(action: {}) {
+          Button(action: {
+            spinReels()
+            checkWinning()
+            isGameOver()
+          }) {
             Image("gfx-spin")
               .renderingMode(.original)
               .resizable()
@@ -99,16 +164,18 @@ struct ContentView: View {
         HStack {
           // MARK: Bet 20
           HStack(spacing: 10) {
-            Button(action: {}) {
+            Button(action: {
+              activateBet(20)
+            }) {
               Text("20")
                 .fontWeight(.heavy)
-                .foregroundColor(.white)
+                .foregroundColor(isActiveBet20 ? .colorYellow : .white)
                 .modifier(BetNumberModifier())
             }
             .modifier(BetCapsuleModifier())
             Image("gfx-casino-chips")
               .resizable()
-              .opacity(0)
+              .opacity(isActiveBet20 ? 1: 0)
               .modifier(CasinoChipsModifier())
           }
           
@@ -116,12 +183,14 @@ struct ContentView: View {
           HStack(spacing: 10) {
             Image("gfx-casino-chips")
               .resizable()
-              .opacity(1)
+              .opacity(isActiveBet10 ? 1: 0)
               .modifier(CasinoChipsModifier())
-            Button(action: {}) {
+            Button(action: {
+              activateBet(10)
+            }) {
               Text("10")
                 .fontWeight(.heavy)
-                .foregroundColor(.yellow)
+                .foregroundColor(isActiveBet10 ? .colorYellow : .white)
                 .modifier(BetNumberModifier())
             }
             .modifier(BetCapsuleModifier())
@@ -137,7 +206,9 @@ struct ContentView: View {
         alignment: .topLeading
       ) // :Reset button
       .overlay(
-        Button(action: {}) {
+        Button(action: {
+          showingInfoView = true
+        }) {
           Image(systemName: "info.circle")
         }
         .modifier(ButtonModifier()),
@@ -145,6 +216,63 @@ struct ContentView: View {
       ) // :Info button
       .padding()
       .frame(maxWidth: 720)
+      .blur(radius: showingModal ? 5: 0, opaque: false)
+      
+      // MARK: Popup
+      if showingModal {
+        ZStack {
+          Color.colorTransparentBlack
+            .edgesIgnoringSafeArea(.all)
+          VStack(spacing: 0) {
+            Text("GAME OVER")
+              .font(.system(.title, design: .rounded))
+              .fontWeight(.heavy)
+              .padding()
+              .frame(minWidth: 0, maxWidth: .infinity)
+              .background(Color.colorPink)
+              .foregroundColor(.white)
+            Spacer()
+            VStack(spacing: 10) {
+              Image("gfx-seven-reel")
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: 72)
+              Text("Bak luck! You lost all of the coins. \nLet's play again!")
+                .font(.system(.body, design: .rounded))
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+                .layoutPriority(1)
+              Button(action: {
+                showingModal = false
+                coins = 100
+              }) {
+                Text("New Game".uppercased())
+                  .font(.system(.body, design: .rounded))
+                  .fontWeight(.semibold)
+                  .accentColor(.colorPink)
+                  .padding(.horizontal, 12)
+                  .padding(.vertical, 8)
+                  .frame(minWidth: 128)
+                  .background(
+                    Capsule()
+                      .strokeBorder(lineWidth: 1.75)
+                      .foregroundColor(.colorPink)
+                  )
+              }
+            }
+            Spacer()
+          }
+          .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+          .background(Color.white)
+          .cornerRadius(20)
+          .shadow(color: .colorTransparentBlack, radius: 6, x: 0, y: 8 )
+        }
+      }
+      
+    } // :ZStack
+    .sheet(isPresented: $showingInfoView) {
+      InfoView()
     }
   }
 }
---
+++ SlotMachine/Views/InfoView.swift
@@ -0,0 +1,75 @@
+//
+//  InfoView.swift
+//  SlotMachine
+//
+//  Created by Víctor Manuel Puga Ruiz on 21/03/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+  // MARK: - Properties
+  @Environment(\.presentationMode) var presentationMode
+  
+  
+  // MARK: - Body
+  var body: some View {
+    VStack(spacing: 10) {
+      LogoView()
+      Spacer()
+      
+      Form {
+        Section(header: Text("About")) {
+          FormRowView(firstItem: "Application", secondItem: "Slot Machine")
+          FormRowView(firstItem: "Platforms", secondItem: "iPhone, iPad, Mac")
+          FormRowView(firstItem: "Developer", secondItem: "VictorPuga")
+          FormRowView(firstItem: "Designer", secondItem: "Robert Petras")
+          FormRowView(firstItem: "Music", secondItem: "Dan Lebowitz")
+          FormRowView(firstItem: "Website", secondItem: "swiftuimasterclass.com")
+          FormRowView(firstItem: "Copyright", secondItem: "© 2021 All rights reserved.")
+          FormRowView(firstItem: "Version", secondItem: "1.0.0")
+        }
+      }
+      .font(.system(.body, design: .rounded))
+    }
+    .padding(.top, 40)
+    .overlay(
+      Button(action: {
+        presentationMode.wrappedValue.dismiss()
+      }) {
+        Image(systemName: "xmark.circle")
+          .font(.title)
+      }
+      .padding(.top, 30)
+      .padding(.trailing, 20)
+      .accentColor(.secondary)
+      , alignment: .topTrailing
+    )
+  }
+}
+
+// MARK: - Row
+struct FormRowView: View {
+  // MARK: - Properties
+  let firstItem: String
+  
+  let secondItem: String
+  
+  // MARK: - Body
+  var body: some View {
+    HStack {
+      Text(firstItem)
+        .foregroundColor(.gray)
+      Spacer()
+      Text(secondItem)
+    }
+  }
+}
+
+// MARK: - Preview
+struct InfoView_Previews: PreviewProvider {
+  static var previews: some View {
+    InfoView()
+  }
+}
+
